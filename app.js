031c2c91491b1f8fbf12c2d6bbf061ae1c22ba5d
Jack Parker <parkej3@gmail.com>
2012-06-18 21:57:23 -0400

    changed port to 8080

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var server = http.createServer(app);
 *************************************************************/
 // Global Config
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
